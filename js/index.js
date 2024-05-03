57abf8d3cd09b4583d8776f11282715f3bc47a42
Arpit <arpit.mishra.out@gmail.com>
2024-05-03 18:52:11 +0530

    App Updated:- session storage working

--- js/index.js
+++ js/index.js
@@ -15,18 +15,25 @@ const input = document.getElementById(`task-input`);
 const taskList = document.querySelector(`.task-list`);
 const addBtn = document.getElementById(`add`);
 
-// Object which will store all user entered tasks.
-const taskObj = {};
-
 // state variable
 
+// Object which will store all user entered tasks.
+let taskObj = {};
 // unique id of object storing in session
-let sessionId = sessionStorage.getItem(`num`) || 1; // setting id in storage
+let sessionId = sessionStorage.getItem(`num`) || 0; // setting id in storage
+
+let tasks; // assign when page get loads
 console.log(`session Id before task added:${sessionId}`);
 
 // function to add tasks
 const addTask = function (e) {
   console.log(` starting sessionId when task is added: ${sessionId}`);
+
+  // checking if tasks object is exist when page loads.
+  if (tasks) {
+    console.log(`Page reload after task added!`);
+    taskObj = tasks; // assign tasks object to taskObj
+  }
 
   // *****************************
   // *** task is created *********
@@ -41,7 +48,8 @@ const addTask = function (e) {
   taskObj[`task-${sessionId}`] = `${input.value}`;
 
   // task object
-  console.log(`taskObject: ${taskObj}`);
+  console.log(`Task Object`);
+  console.log(taskObj);
   // Storing all this data in the session storage and local storage in browser
 
   // Storing all data into the session storage.
@@ -54,7 +62,8 @@ const addTask = function (e) {
   console.log(`Length of session storage: ${sessionStorage.length}`);
 
   // printing session storage
-  console.log(`Session storage after task added: ${sessionStorage}`);
+  console.log(`Session storage after task added:`);
+  console.log(sessionStorage);
   // updating session ID
   sessionId++;
 
@@ -67,14 +76,19 @@ addBtn.addEventListener(`click`, addTask);
 
 document.addEventListener(`DOMContentLoaded`, function (e) {
   console.log(sessionStorage);
-  const tasks = JSON.parse(sessionStorage.getItem(`myTask`));
-  console.log(tasks);
+  if (sessionStorage.getItem(`myTask`)) {
+    tasks = JSON.parse(sessionStorage.getItem(`myTask`));
+    console.log(tasks);
 
-  // Showing all these tasks in DOM when the page loads
-  for (const key in tasks) {
-    const task = document.createElement(`li`);
-    task.innerHTML = tasks[key];
-    task.classList.add("task");
-    taskList.append(task);
+    // Showing all these tasks in DOM when the page loads
+    for (const key in tasks) {
+      const task = document.createElement(`li`);
+      task.innerHTML = tasks[key];
+      task.classList.add("task");
+      taskList.append(task);
+    }
   }
+
+  // when page is loaded session id got updated!
+  sessionId++;
 });
